b0c6a4f3cc9920d81e4672d2fff138fe6f431126
KEAIJIANG1234 <1827099971@qq.com>
2026-04-18 10:49:57 +0800

    second commit

---
+++ AAA.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+pd.set_option('display.max_columns',None)
+pd.set_option('display.max_colwidth',None)
+df=pd.read_excel("data.xlsx")
+print("可爱酱很可爱")
+df["D值"]=df["L"]*df["E"]*df["C"]
+pd.set_option('display.unicode.ambiguous_as_wide', True)
+pd.set_option('display.unicode.east_asian_width', True)
+pd.set_option('display.width', 200)
+
+keyword=st.text_input("危险源：")
+result=df[df["危险源"].str.contains(keyword,na=False)]
+if result.empty:
+    print("待扩展....")
+else:          
+    print(result[["危险源","可能后果","危险等级","D值","控制措施"]])
+
